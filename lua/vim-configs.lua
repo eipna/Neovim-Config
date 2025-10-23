764417b2f225c9de7573d610686a91f2b8bddebf
eipna <serbispace@proton.me>
2025-10-24 00:36:08 +0800

    Disabled word wrap

--- lua/vim-configs.lua
+++ lua/vim-configs.lua
@@ -11,6 +11,7 @@ vim.o.termguicolors = true
 vim.o.smartcase = true        
 vim.o.ignorecase = true      
 vim.o.hlsearch = true       
+vim.o.wrap = false
 
 vim.o.tabstop = 4          
 vim.o.shiftwidth = 4      
